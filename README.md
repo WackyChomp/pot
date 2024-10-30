57acfde202f2c34e30aacc8db8043d872a9af901
Wilson Chan <wilsonchan54@gmail.com>
2024-10-29 22:34:35 -0400

    Added form and buttons to SignInCard component / updated README.md

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 <br />
 <div align="center" id='readme-top'>
   <img src="https://i.ibb.co/k82fmLJ/Alarm-Clock-GIF-Animation-High-Res.gif" alt="Logo" target='blank' width="100" height="80">
-  <img src="https://i.ibb.co/7yKqKgL/ct9NpkI.gif" alt="Logo" target='blank' width="150" height="100">
+  <img src="https://i.ibb.co/k82fmLJ/Alarm-Clock-GIF-Animation-High-Res.gif" alt="Logo" target='blank' width="100" height="80">
   <img src="https://i.ibb.co/k82fmLJ/Alarm-Clock-GIF-Animation-High-Res.gif" alt="Logo" target='blank' width="100" height="80">
 
   <h3 align="center">Projects On Time</h3>
@@ -23,3 +23,26 @@
     <a href="https://github.com/WackyChomp/pot/issues">Request Feature</a>
   </p>
 </div>
+
+
+
+<!-- BUILT WITH -->
+## Built With
+Here are some frameworks/libraries used in this project:
+* [![TypeScript][TypeScript]][TypeScript-url]
+* [![Tailwind][Tailwind.css]][Tailwind-url]
+* [![React][React.js]][React-url]
+* [![Next][Next.js]][Next-url]
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+[Next.js]: https://img.shields.io/badge/next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white
+[Next-url]: https://nextjs.org/
+[TypeScript]: https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white
+[TypeScript-url]: https://www.typescriptlang.org/
+
+[React.js]: https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB
+[React-url]: https://reactjs.org/
+[Tailwind.css]: https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white
+[Tailwind-url]: https://tailwindcss.com/
--- src/features/auth/components/SignInCard.tsx
+++ src/features/auth/components/SignInCard.tsx
@@ -1,10 +1,17 @@
+import Image from 'next/image'
+
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 
 import { DottedSeparator } from '@/components/dotted-separator'
 
+import { FcGoogle } from 'react-icons/fc'
+import { FaGithub } from 'react-icons/fa'
+
 export const SignInCard = () =>{
+  const geminiIcon = `data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWNyb3NzIj48cGF0aCBkPSJNMTEgMmEyIDIgMCAwIDAtMiAydjVINGEyIDIgMCAwIDAtMiAydjJjMCAxLjEuOSAyIDIgMmg1djVjMCAxLjEuOSAyIDIgMmgyYTIgMiAwIDAgMCAyLTJ2LTVoNWEyIDIgMCAwIDAgMi0ydi0yYTIgMiAwIDAgMC0yLTJoLTVWNGEyIDIgMCAwIDAtMi0yaC0yeiIvPjwvc3ZnPg==`
+
   return(
     <Card className="w-full h-full md:w-[490px] border-none shadow-none">
       <CardHeader className='flex items-center justify-center text-center p-10'>
@@ -16,6 +23,52 @@ export const SignInCard = () =>{
       <div className="px-12">
         <DottedSeparator />
       </div>
+      
+      <CardContent className="p-12">
+        <form className='space-y-4'>
+          <Input 
+            required
+            type='email'
+            value={''}
+            onChange={() => {}}
+            placeholder='Enter your email address'
+            disabled={false}
+          />
+          <Input 
+            required
+            type='password'
+            value={''}
+            onChange={() => {}}
+            placeholder='Enter your password'
+            disabled={false}
+            min={8}
+            max={256}
+          />
+          <Button disabled={false} variant='mystery' size='lg' className='w-full'>
+            Login
+          </Button>
+        </form>
+      </CardContent>
+
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+
+      <CardContent className='flex flex-col p-7 gap-y-10'>
+        <Button disabled={false} variant='mystery' size='lg' className='w-full'>
+          <FcGoogle className='mr-2 size-5' />
+          Login with GooGoo
+        </Button>
+        <Button disabled={false} variant='mystery' size='lg' className='w-full'>
+          <FaGithub className='text-green-600 mr-2 size-5' />
+          Login with GitHub
+        </Button>
+        <Button disabled={false} variant='mystery' size='lg' className='w-full animate-bounce'>
+          <Image src={geminiIcon} alt='icon' width={15} height={15} className='bg-yellow-400 rounded-lg animate-spin' />
+          Login with Gemini<span className='text-[7px] -rotate-6 p-4 scale-x-[-1]'>{`(how does that work)?`}</span>
+        </Button>
+      </CardContent>
+
     </Card>
   )
 }
