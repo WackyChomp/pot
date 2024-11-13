71d16f44ed908063d144f280c4f348bf7b3e8498
Wilson Chan <wilsonchan54@gmail.com>
2024-11-12 23:53:12 -0500

    Called appwrite createAdminClient and cookie session for account sign-in and create logout post request in server route.ts

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { z } from 'zod';
 import { zValidator } from '@hono/zod-validator';     // middleware
-import { setCookie } from 'hono/cookie'
+import { deleteCookie, setCookie } from 'hono/cookie'
 import { loginSchema, registerSchema } from "../schemas";
 
 import { createAdminClient } from "@/lib/appwrite";
@@ -15,8 +15,22 @@ const app = new Hono()
     async (c) => {
       const { email, password } = c.req.valid('json')
 
+      const { account } = await createAdminClient();
+      const session = await account.createEmailPasswordSession(
+        email,
+        password,
+      );
+
+      setCookie(c, AUTH_COOKIE, session.secret, {
+        path: '/',
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 60 * 60 * 24 * 30,
+      })
+
       console.log({ email, password})
-      return c.json({ success: 'login is actually working <(;o_o;)>', email, password})
+      return c.json({ success: `login is actually working <(;o_o;)> ${true}`, email, password})
     }
   )
   .post(
@@ -47,8 +61,14 @@ const app = new Hono()
       })
 
       console.log({ name, email, password })
-      return c.json({ data: user, succes:'registration really worked! (O__o)', name, email, password })
+      return c.json({ data: user, succes: `registration really worked! (O__o) ${true}`, name, email, password })
     }
   )
+
+  .post('/logout', (c) => {
+    deleteCookie(c, AUTH_COOKIE);
+
+    return c.json({ success: `coooookie is eviscerated ${true}`})
+  })
 
 export default app;
