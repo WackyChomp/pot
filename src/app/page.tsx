31e2b86e54626dc859bf0af23a72be04f13c965a
Wilson Chan <wilsonchan54@gmail.com>
2024-11-18 22:37:38 -0500

    Removed useEffect sign-in page redirect to replace with actions.ts in app page.tsx / moved button components from app page.tsx to features Test.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,33 @@
-'use client'
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-import Test from "@/features/Test";
+import { Button } from "@/components/ui/button";
 import { UserButton } from "@/features/auth/components/UserButton";
+import Test from "@/features/Test";
+import { getCurrent } from "@/features/auth/actions";
 
 //import { createAdminClient } from "@/lib/appwrite";          // testing to see this in console means server-only isn't working in appwrite.ts
-import { useCurrent } from "@/features/auth/api/use-current";
-import { useLogout } from "@/features/auth/api/use-logout";
 
-export default function Home() {
+export default async function Home() {
   //console.log(createAdminClient, 'why are you here?');      // testing to see this in console means server-only isn't working in appwrite.ts
 
-  {/*
-  const router = useRouter();
-  const { data, isLoading } = useCurrent();
-  const { mutate } = useLogout();
-
-  useEffect(() => {             // logging out: useCurrent becomes null and this triggers
-    if (!data && !isLoading){
-      router.push('/sign-in')
-    }
-  }, [data])
-  */}
+  const user = getCurrent();
+  if (!user) redirect('/sign-in')
 
   return (
     <div className="bg-blue-500 flex justify-center">
 
       <div className="bg-green-600 flex flex-col">
 
-      <div className="flex flex-col mx-auto items-center">
+      <div className="flex flex-col mx-auto items-center gap-2">
         <div className="">POT ON TIME</div>
         <Test />
-        <Button><Link href='/sign-in'>Sign In</Link></Button>
-        <Button><Link href='/sign-up'>Sign Up</Link></Button>
+        <div className="flex gap-3">
+          <Button><Link href='/sign-in'>Sign In</Link></Button>
+          <Button><Link href='/sign-up'>Sign Up</Link></Button>
+        </div>
       </div>
-
-      <div className="flex items-center gap-4 p-4">
-        <Button variant='default'>Button #1</Button>
-        <Button variant='destructive' size='sm'>Button #2</Button>
-        <Button variant='outline' size='lg'>Button #3</Button>
-        <Button variant='secondary' size='sm'>Button #4</Button>
-        <Button variant='ghost' size='lg'>Button #5</Button>
-        <Button variant='mystery' size='xs'>Button #6</Button>
-      </div>
-
-      <Input />
-
-      <Button onClick={() => mutate()}>Logout Now</Button>    {/* uses mutate and useEffect , need to uncomment above */}
 
       <UserButton />
       </div>
--- src/features/Test.tsx
+++ src/features/Test.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 type Props = {}
 
 const Test = (props: Props) => {
   return (
+    <div className="bg-purple-600 flex flex-col mx-auto items-center">
+
     <div className='text-yellow-400 font-bold'>Test is all I do here</div>
+
+    <div className="flex items-center gap-4 p-4">
+      <Button variant='default'>Button #1</Button>
+      <Button variant='destructive' size='sm'>Button #2</Button>
+      <Button variant='outline' size='lg'>Button #3</Button>
+      <Button variant='secondary' size='sm'>Button #4</Button>
+      <Button variant='ghost' size='lg'>Button #5</Button>
+      <Button variant='mystery' size='xs'>Button #6</Button>
+    </div>
+
+    </div>
   )
 }
 
